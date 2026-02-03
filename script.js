74bac94490574c4953dbabc872a93aa08e5c1552
Haim Daniela Flalo <kvim.hda@gmail.com>
2026-02-03 23:36:21 +0200

    hello

--- script.js
+++ script.js
@@ -1,39 +1,40 @@
 function getStoneType() {
-  const types = ['redstone', 'coal', 'stones', 'diamond', 'gold'];
+  const types = ["redstone", "coal", "stones", "diamond", "gold"];
   return types[Math.floor(Math.random() * types.length)];
 }
 
 function genBoard() {
-  const board = document.getElementById('game-container');
-  const items = document.getElementById('items');
+  const board = document.getElementById("game-container");
+  const items = document.getElementById("items");
   for (let i = 0; i < 25; i++) {
     for (let j = 0; j < 40; j++) {
-      const tile = document.createElement('div');
-      tile.className = 'sky';
+      const tile = document.createElement("div");
+      tile.className = "sky";
       if (i >= 19) {
         if (i === 19 && j < 4) {
-          tile.className = 'soil';
+          tile.className = "soil";
         } else {
-          tile.className = 'stone';
+          tile.className = "stone";
           tile.id = getStoneType();
         }
       } else if (i >= 15) {
-        tile.className = 'soil';
+        tile.className = "soil";
         if ((i === 15 && j > 13) || (i === 16 && j < 14)) {
-          tile.className = 'grass';
+          tile.className = "grass";
         }
         if (i === 15 && j < 14) {
-          tile.className = 'sky';
+          tile.className = "sky";
         }
       }
-      tile.addEventListener('click', (event) => {
-        const item = document.createElement('div');
+      tile.addEventListener("click", (event) => {
+        const item = document.createElement("div");
         item.id = event.target.id;
         item.className = event.target.className;
-        if (event.target.id !== 'sky') {
+        if (event.target.id !== "sky") {
+        if(items.include event.target.id){}
           items.append(item);
-          tile.className = 'sky';
-          tile.id = 'sky';
+          tile.className = "sky";
+          tile.id = "sky";
         }
       });
       board.appendChild(tile);
@@ -41,36 +42,32 @@ function genBoard() {
   }
 }
 
-const reset = document.getElementById('reset');
-reset.addEventListener('click', () => {
+const reset = document.getElementById("reset");
+reset.addEventListener("click", () => {
   window.location.reload();
 });
 
 function change() {
-  const ax = document.getElementById('ax');
-  const shovel = document.getElementById('shovel');
-  const sword = document.getElementById('sword');
-  const pickaxe = document.getElementById('pickaxe');
-  ax.addEventListener('click', () => {
-    document.body.style.cursor = "url('images/ax.ico'), none";
-  });
-  shovel.addEventListener('click', () => {
-    document.body.style.cursor = "url('images/shovel.ico'), none";
-  });
-  sword.addEventListener('click', () => {
-    document.body.style.cursor = "url('images/sword.ico'), none";
-  });
-  pickaxe.addEventListener('click', () => {
-    document.body.style.cursor = "url('images/pickaxe.ico'), none";
+  const tools = document.querySelectorAll("#tools > *");
+  tools.forEach((tool) => {
+    tool.addEventListener("click", () => {
+      if (tool.id) {
+        document.body.style.cursor = `url('images/${tool.id}.ico'), auto`;
+      }
+    });
   });
 }
 
 function getItems() {
-  box.addEventListener('click', () => {
-    if (items.style.display === 'none' || items.style.display === '') {
-      items.style.display = 'block';
+  box.addEventListener("click", () => {
+    if (items.style.display === "none" || items.style.display === "") {
+      console.log(items.length);
+      if (items.length === 0) {
+        items.innerHTML = "empty";
+      }
+      items.style.display = "grid";
     } else {
-      items.style.display = 'none';
+      items.style.display = "none";
     }
   });
 }
